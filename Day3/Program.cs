000e159dd7d23584bcee3ab50e43fa33ea051bcc
Nick Verhaegen <gatzby@gmail.com>
2022-12-03 14:29:05 +0100

    added part2 of day 3

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -2,20 +2,26 @@
 
 var dataset = File.ReadAllLines("C:\\Dev\\Aoc-2022\\Day3\\data\\prod.txt");
 
-Func<char,int> getScore = x => (x < 91 ? x - 38 : x - 96 );
+Func<char,int> getPriority = x => (x < 91 ? x - 38 : x - 96 );
 
 int totalseverity = 0;
+int elfpackseverity = 0;
+int i = 0;
+List<List<char>> elfpack = new List<List<char>>();
+
 
 foreach (var line in dataset)
 {
-    List<char> firsthalf = line.Substring(0,line.Length / 2).ToList<char>();
-    List<char> secondhalf = line.Substring(line.Length / 2, line.Length / 2).ToList<char>();
-    var commonitems = firsthalf.Intersect(secondhalf);
+    i++;
+    elfpack.Add(line.ToList<char>());
+    totalseverity += getPriority(line.Substring(0, line.Length / 2).ToList<char>().Intersect(line.Substring(line.Length / 2, line.Length / 2).ToList<char>()).FirstOrDefault());
 
-    foreach (var item in commonitems)
+    if (i % 3 == 0)
     {
-        totalseverity += getScore(item); ;
+        elfpackseverity += getPriority(elfpack[0].Intersect(elfpack[1]).Intersect(elfpack[2]).FirstOrDefault());
+        elfpack.Clear();
     }
 }
 
 Console.WriteLine($"Total severity is {totalseverity}");
+Console.WriteLine($"Total elf priority sum is {elfpackseverity}");
