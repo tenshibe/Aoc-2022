1f564863c1d4c56bd17a2a837f9b0d79d96a7b92
Nick Verhaegen <gatzby@gmail.com>
2022-12-07 04:24:03 +0100

    Day4

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,8 +1,28 @@
 ﻿Console.WriteLine("Advent Of Code 2022 Day4");
 
-var dataset = File.ReadAllLines("C:\\Dev\\Aoc-2022\\Day4\\data\\test.txt");
+var dataset = File.ReadAllLines("C:\\Dev\\Aoc-2022\\Day4\\data\\prod.txt");
+
+var overlaps = 0;
+var pairoverlaps = 0;
 
 foreach (var line in dataset)
 {
+    var elfs = line.Split(',');
+    var elf1bounds = elfs[0].Split("-");
+    var elf2bounds = elfs[1].Split("-");
+    if ( ((int.Parse(elf1bounds[0]) <= int.Parse(elf2bounds[0])) && (int.Parse(elf1bounds[1]) >= int.Parse(elf2bounds[1]))) 
+         || ((int.Parse(elf2bounds[0]) <= int.Parse(elf1bounds[0])) && (int.Parse(elf2bounds[1]) >= int.Parse(elf1bounds[1])))
+        )
+    {
+        overlaps++;
+    }
+    if ( int.Parse(elf1bounds[1]) >= int.Parse(elf2bounds[0]) &&  int.Parse(elf1bounds[0]) <= int.Parse(elf2bounds[1]) )
+        
+    {
+        pairoverlaps++;
+    }
 
 }
+
+Console.WriteLine($"We have {overlaps} overlaps");
+Console.WriteLine($"We have {pairoverlaps} pair overlaps");
---
+++ Day5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Advent Of Code 2022 Day5");
+
+var dataset = File.ReadAllLines("C:\\Dev\\Aoc-2022\\Day5\\data\\test.txt");
+
+foreach (var line in dataset)
+{
+
+}
